cea08384673d4c1516b228453f6db1e9dff86784
Lincoln Kermit <104798220+LincolnKermit@users.noreply.github.com>
2023-11-06 09:49:36 +0100

    Update clean.sh | Fixes Bugs for alias and cp

--- clean.sh
+++ clean.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 user=$SUDO_USER
 clear
+echo "[]"
+sleep 0.5
+clear
+script_name="clean.sh"
+if [ -f "$script_name" ]; then
+    sudo cp clean.sh /usr/local/bin/
+    sudo chmod +x /usr/local/bin/clean.sh
+    echo 'alias clean="/usr/local/bin/clean.sh"' >> ~/.zshrc
+    source ~/.zshrc
+
+    echo "Le script $script_name a été copié dans /usr/local/bin et un alias permanent a été créé."
+else
+    echo "Le script $script_name n'a pas été trouvé dans le répertoire actuel."
+fi
+sleep 0.5
+clear
 echo "[*]"
+sleep 0.5
+clear
+echo "[]"
+sleep 0.5
 rm -rf "/Users/$user/Library/Caches"/*
 rm -rf "/Users/$user/Library/Containers/com.apple.Safari/Data/Library/Caches/"*
-alias clean="sudo bash /Users/$user/Documents/Workspace/clean.sh"
+rm -rf "/tmp/"*
+echo "Erased most temps files at : "
+echo "/Users/$user/Library/Caches/*"
+echo "/Users/$user/Library/Containers/com.apple.Safari/Data/Library/Caches/*"
+echo "/tmp/*"
+sleep 0.5
 clear
 echo "[*]"
